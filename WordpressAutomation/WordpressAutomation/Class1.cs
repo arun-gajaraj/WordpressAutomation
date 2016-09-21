4ca81e7c34f25b90a215568fa5c03606843ce57e
arunamigo@live.com <arunamigo@live.com>
2016-09-21 08:37:01 -0700

    files added!

--- WordpressAutomation/WordpressAutomation/Class1.cs
+++ WordpressAutomation/WordpressAutomation/Class1.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,18 @@ namespace WordpressAutomation
     {
         public void Go() {
             //var driver = new FirefoxDriver();
-            var driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver(@"E:\Work\Selenium\CSharp");
 
-            driver.Navigate().GoToUrl("http://localhost:8080/wp/wp-login.php");
+            driver.Navigate().GoToUrl(@"http://localhost:8080/wp/wp-login.php");
             //driver.Navigate().GoToUrl("http://google.com");
+
+            //driver.go
         }
+
+        public static void Main() {
+            var cl = new Class1();
+            cl.Go();
+        }
+        
     }
 }
