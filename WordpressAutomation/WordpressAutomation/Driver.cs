edde7b5cdd56aef8d1413da5774d1d7fa8527764
arunamigo@live.com <arunamigo@live.com>
2016-09-21 23:22:56 -0700

    Init

---
+++ WordpressAutomation/WordpressAutomation/Driver.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WordpressAutomation
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static IWebDriver Initialize() {
+            Instance = new ChromeDriver(@"E:\Work\Selenium\CSharp");
+            Instance.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(5));
+            return Instance;
+        }
+
+
+    }
+}
